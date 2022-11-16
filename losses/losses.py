0f793c36430e406fcefeb021ce319297df57fe59
lucas <hooftlucas@gmail.com>
2022-11-16 10:04:53 +0100

    mse function

--- losses/losses.py
+++ losses/losses.py
@@ -4,7 +4,7 @@ import utilities.utils as utils
 
 
 def mse(input_tensor: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
-    err= target - input_tensor
-    squared_err = err *err
-    mean = torch.mean(squared_err)
+    err= target - input_tensor # (y-yi)
+    squared_err = err *err #take the square of the err
+    mean = torch.mean(squared_err)  #mean value
     return mean
